eeb408a4508c374d1519cbc3dae546f2fd9268b7
Richie <richie_github@grange-lane.co.uk>
2025-10-02 00:47:48 +0100

    Update fir.c

--- Project Files/Source/wdsp/fir.c
+++ Project Files/Source/wdsp/fir.c
@@ -379,8 +379,9 @@ void mp_imp (int N, double* fir, double* mpfir, int pfactor, int polarity)
 	double* imp = get_impulse_cache_entry(MP_CACHE, h, N);
 	if (imp)
 	{
-		memcpy(mpfir, imp, N * sizeof(complex)); // need to copy into mpfir  //comment to verify cache operation
-		_aligned_free(imp); // free the cache return value as data copied into mpfir  //comment to verify cache operation
+		//comment to verify cache operation - comment the following 3 lines if testing of cached vs calculated mpfir is required
+		memcpy(mpfir, imp, N * sizeof(complex)); // need to copy into mpfir  
+		_aligned_free(imp); // free the cache return value as data copied into mpfir as we are done with this
 		return;  //comment to verify cache operation
 	}
 	//
@@ -433,10 +434,11 @@ void mp_imp (int N, double* fir, double* mpfir, int pfactor, int polarity)
 	_aligned_free (firfreq);
 	_aligned_free (firpad);
 
+	//comment to verify cache operation - comment this next line (add_impulse...) if cache verification testing is required
 	// store in cache
 	add_impulse_to_cache(MP_CACHE, h, N, mpfir);  //comment to verify cache operation
 	
-	// debug test code, comment out areas of code above marked with //comment to verify cache operation
+	// debug test code, comment out areas of code above see //comment to verify cache operation
 	// and uncomment this block to verify cache operation
 	//
 	//if (imp == NULL)
